fe96f23d11c3137c720ab69a15a06ba8b5bdeb76
Corbin Sykes <corbin.sykes@gmail.comm>
2013-08-05 14:14:37 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jammin::Application.config.secret_token = '29c93e0ff40bbe585ce762b7c4a39c5179a62debfc16d8f56045cfbcde2b568a1f9b0849b030bcaf6d6bc1cde80265f45f3ddffd1116e614686db4f397c171cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Jammin::Application.config.session_store :cookie_store, key: '_jammin_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Jammin::Application.config.session_store :active_record_store
