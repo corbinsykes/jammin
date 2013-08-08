3846ab8d12bbd81958a807629fdbc9ebf805a255
Corbin Sykes <corbin.sykes@gmail.comm>
2013-08-08 14:37:13 -0400

    About to add active model serializers

--- app/controllers/jams_controller.rb
+++ app/controllers/jams_controller.rb
@@ -1,6 +1,10 @@
 class JamsController < ApplicationController
   def index
     @jams = Jam.all
+    respond_to do|format|
+      format.html
+      format.json {render json: @jams}
+    end
   end
 
   def join
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,23 @@
-Musician.create(firstname: 'Sample1', lastname: 'Person A', email: 'fake1@example.com', instrument: 'viola')
-Musician.create(firstname: 'Sample2', lastname: 'Person B', email: 'fake2@example.com', instrument: 'cello')
-Musician.create(firstname: 'Sample3', lastname: 'Person C', email: 'fake3@example.com', instrument: 'guitar')
+musician1 = Musician.create(firstname: 'Sample1', lastname: 'Person A', email: 'fake1@example.com', instrument: 'viola')
+musician2 = Musician.create(firstname: 'Sample2', lastname: 'Person B', email: 'fake2@example.com', instrument: 'cello')
+musician3 = Musician.create(firstname: 'Sample3', lastname: 'Person C', email: 'fake3@example.com', instrument: 'guitar')
 
-Venue.create(address: '110 5th Ave')
+venue = Venue.create(address: '110 5th Ave')
 
-Instrument.create(name: 'Guitar')
-Instrument.create(name: 'Bass')
-Instrument.create(name: 'Drums')
-Instrument.create(name: 'Vocals')
+instrument1 = Instrument.create(name: 'Guitar')
+instrument2 = Instrument.create(name: 'Bass')
+instrument3 = Instrument.create(name: 'Drums')
+instrument4 = Instrument.create(name: 'Vocals')
+
+jam = Jam.create
+jam.musicians << musician1
+jam.musicians << musician2
+jam.musicians << musician3
+
+jam.instruments << instrument1
+jam.instruments << instrument2
+jam.instruments << instrument3
+jam.instruments << instrument4
+
+jam.venue = venue
+jam.save
