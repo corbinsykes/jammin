88b1aa5eb2a3b72ab487a0ebb7c912e046c621c3
Ben Israeli <jisraeli@gmail.com>
2013-08-08 14:20:45 -0400

    updated map style

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,5 +1,7 @@
 window.onload = function(){
-  var mapOptions = {};
+  var mapOptions = {zoom: 12,
+    center: new google.maps.LatLng(40.699121, -73.911579),
+    mapTypeId: google.maps.MapTypeId.ROADMAP};
 
   var map = new google.maps.Map(document.getElementById("map"),
     mapOptions);
