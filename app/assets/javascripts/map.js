419081988fc728a655497fe9ba5ddea3e88ca6c2
Corbin Sykes <corbin.sykes@gmail.comm>
2013-08-08 14:20:42 -0400

    functional google map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,5 +1,8 @@
 window.onload = function(){
-  var mapOptions = {};
+  var mapOptions = {
+    center: new google.maps.LatLng(40.699121, -73.911579),
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP};
 
   var map = new google.maps.Map(document.getElementById("map"),
     mapOptions);
