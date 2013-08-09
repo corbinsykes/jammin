060a6582e911fc0a0b718c4f78721eb2ab26c1a1
Corbin Sykes <corbin.sykes@gmail.comm>
2013-08-08 22:45:19 -0400

    map markers

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,19 +1,33 @@
+var map;
+
 window.onload = function(){
 
   var mapOptions = {
     center: new google.maps.LatLng(40.70771, -73.940398),
     zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP};
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
 
-  var map = new google.maps.Map(document.getElementById("map"),
+  map = new google.maps.Map(document.getElementById("map"),
     mapOptions);
-
-  var marker = new google.maps.Marker(markerOptions);
 
   var markerOptions = {
     position: new google.maps.LatLng(40.70771, -73.940398),
     visible: true
   };
+  var marker = new google.maps.Marker(markerOptions);
+  marker.setMap(map);
 };
+
+function addMarkers(venues){
+  for (var i = 0; i < max; i++){
+    var markerOptions = {
+      position: new google.maps.LatLng(40.70771, -73.940398),
+      visible: true
+    };
+    var marker = new google.maps.Marker(markerOptions);
+      marker.setMap(map);
+  }
+}
 
 
